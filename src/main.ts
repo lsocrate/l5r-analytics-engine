329dd4da79df4d1f52ab6d7b0cd8d6b8e3d4663e
Luiz Socrate <luiz@socrate.com.br>
2023-05-29 17:54:56 +0200

    Report endpoint

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,35 @@ const router = new Router();
 router.post("/api/game-report", createGameReportOld(db));
 router.post("/api/game-report/:env", createGameReport(db));
 router.get("/api/player-stats", listPlayerStats(db));
+router.get("/api/game-stats", async (context) => {
+  const gamesPerDay = await db.reportCollectionV2
+    .aggregate([
+      { $unionWith: "reports_v1" },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              date: "$startedAt",
+              format: "%Y-%m-%d :: %u",
+              timezone: "-08",
+            },
+          },
+          gamesOnDay: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          day: "$_id",
+          gamesOnDay: 1,
+        },
+      },
+      { $sort: { day: 1 } },
+    ])
+    .toArray();
+  context.response.body = gamesPerDay;
+});
+
 // router.post("/api/backfill", async () => {
 //   await backfillStats(db);
 // });
